bce95d429ed7431ee92e13d7e59e911c7c54784d
Jeremy Fyke <fyke@lanl.gov>
2015-08-21 12:33:12 -0600

    Added GPL lisence to all files, and included full lisence text in separate file.

--- calculate_historical_reserve_growth_rate.m
+++ calculate_historical_reserve_growth_rate.m
@@ -1,3 +1,21 @@
+%     Cumulative Emissions Projection Model (CEPM).
+%     Copyright (C) 2015 Jeremy Fyke
+%   
+%     This file is part of CEPM.
+% 
+%     CEPM is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     CEPM is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with CEPM.  If not, see <http://www.gnu.org/licenses/>.
+
 function [mean_discovery,min_discovery,max_discovery,average_emission_factor]=calculate_historical_reserve_growth_rate(c)
 
 %This function returns:
--- emissions_economy_ensemble_runner.m
+++ emissions_economy_ensemble_runner.m
@@ -1,3 +1,20 @@
+%     Cumulative Emissions Projection Model (CEPM).
+%     Copyright (C) 2015 Jeremy Fyke
+%   
+%     This file is part of CEPM.
+% 
+%     CEPM is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     CEPM is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with CEPM.  If not, see <http://www.gnu.org/licenses/
 
 %% Run energy crossover model multiple times for a range of parameters.
 %% Plot resulting spread in crossover times and cumulative emissions.
